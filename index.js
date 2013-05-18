ede349db90f78ef2d7879604b4ca3a7b450b0c46
jesse keane <me@jessekeane.me>
2013-05-19 02:02:00 +0400

    enumerate routes

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var ramrod = require('ramrod')(),
+var router = require('ramrod')(),
   http = require('http'),
   couchdb = require('felix-couchdb'),
   client = couchdb.createClient(5984, 'localhost'),
   db = client.db('redwah');
 
+// Enumerate routes
 
+['list', 'item'].forEach(function (obj) {
+  ['post', 'get', 'put', 'del'].forEach(function (method) {
+    router[method](obj, method + obj);
+  });
+});
 
 db.create(function (err) {
   if (err) { throw err; }
 });
 
 http.createServer(function (req, res) {
-  ramrod.dispatch(req, res);
+  router.dispatch(req, res);
 }).listen(3000);
